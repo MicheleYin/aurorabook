18f035bb03862ab920753b74bf72d75c448b7300
Michele Yin <m.yin@reply.it>
2025-12-13 19:54:22 +0100

    feat: missing false

--- src/components/reader/ReaderAudioPlayer.tsx
+++ src/components/reader/ReaderAudioPlayer.tsx
@@ -1018,6 +1018,7 @@ export function ReaderAudioPlayer({
           trackId: track.id,
         });
         trackChangeInProgressRef.current = false;
+        setIsLocalTrackChanging(false);
       }
     };
     
